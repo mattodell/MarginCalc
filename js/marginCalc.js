86f543586814d0cf00f29b3fc4dffc1730c97b75
mattodell <mrmattodell@gmail.com>
2014-11-25 08:08:53 -0500

    added getInt and getFloat methods to return 0 instead of NaN

--- js/marginCalc.js
+++ js/marginCalc.js
@@ -2,38 +2,60 @@ function addAnotherOption() {
     $("#option2").show(1000);
 }
 
+function getInt(value) {
+    var num = parseInt(value);
+    if (isNaN(num)) {
+        return 0;
+    }
+    else {
+        return num;
+    }
+}
+
+function getFloat(value) {
+    var num = parseFloat(value);
+    if (isNaN(num)) {
+        return 0;
+    }
+    else {
+        return num;
+    }
+}
+
+
+
 function calculateAll(fieldID) {
-    if (fieldID == "mgn_l1") {
+    if (fieldID === "mgn_l1") {
          fillAllSalesMargin();
     }
 
-    if (fieldID == "discount_l1_o1") {
+    if (fieldID === "discount_l1_o1") {
         fillAllDiscount();
     }
 
-    var net_l1_o1 = (parseFloat($("#list_l1_o1").val())) * (1 - (parseFloat($("#discount_l1_o1").val()) / 100));
-    var net_l2_o1 = (parseFloat($("#list_l2_o1").val())) * (1 - (parseFloat($("#discount_l2_o1").val()) / 100));
-    var net_l3_o1 = (parseFloat($("#list_l3_o1").val())) * (1 - (parseFloat($("#discount_l3_o1").val()) / 100));
-    var net_l4_o1 = (parseFloat($("#list_l4_o1").val())) * (1 - (parseFloat($("#discount_l4_o1").val()) / 100));
-    var net_l5_o1 = (parseFloat($("#list_l5_o1").val())) * (1 - (parseFloat($("#discount_l5_o1").val()) / 100));
-    var net_l6_o1 = (parseFloat($("#list_l6_o1").val())) * (1 - (parseFloat($("#discount_l6_o1").val()) / 100));
-    var net_l7_o1 = (parseFloat($("#list_l7_o1").val())) * (1 - (parseFloat($("#discount_l7_o1").val()) / 100));
+    var net_l1_o1 = (getFloat($("#list_l1_o1").val())) * (1 - (getFloat($("#discount_l1_o1").val()) / 100));
+    var net_l2_o1 = (getFloat($("#list_l2_o1").val())) * (1 - (getFloat($("#discount_l2_o1").val()) / 100));
+    var net_l3_o1 = (getFloat($("#list_l3_o1").val())) * (1 - (getFloat($("#discount_l3_o1").val()) / 100));
+    var net_l4_o1 = (getFloat($("#list_l4_o1").val())) * (1 - (getFloat($("#discount_l4_o1").val()) / 100));
+    var net_l5_o1 = (getFloat($("#list_l5_o1").val())) * (1 - (getFloat($("#discount_l5_o1").val()) / 100));
+    var net_l6_o1 = (getFloat($("#list_l6_o1").val())) * (1 - (getFloat($("#discount_l6_o1").val()) / 100));
+    var net_l7_o1 = (getFloat($("#list_l7_o1").val())) * (1 - (getFloat($("#discount_l7_o1").val()) / 100));
 
-    var retail_l1_o1 = net_l1_o1 * (1 + (parseFloat($("#mgn_l1").val()) / 100));
-    var retail_l2_o1 = net_l2_o1 * (1 + (parseFloat($("#mgn_l2").val()) / 100));
-    var retail_l3_o1 = net_l3_o1 * (1 + (parseFloat($("#mgn_l3").val()) / 100));
-    var retail_l4_o1 = net_l4_o1 * (1 + (parseFloat($("#mgn_l4").val()) / 100));
-    var retail_l5_o1 = net_l5_o1 * (1 + (parseFloat($("#mgn_l5").val()) / 100));
-    var retail_l6_o1 = net_l6_o1 * (1 + (parseFloat($("#mgn_l6").val()) / 100));
-    var retail_l7_o1 = net_l7_o1 * (1 + (parseFloat($("#mgn_l7").val()) / 100));
+    var retail_l1_o1 = net_l1_o1 * (1 + (getFloat($("#mgn_l1").val()) / 100));
+    var retail_l2_o1 = net_l2_o1 * (1 + (getFloat($("#mgn_l2").val()) / 100));
+    var retail_l3_o1 = net_l3_o1 * (1 + (getFloat($("#mgn_l3").val()) / 100));
+    var retail_l4_o1 = net_l4_o1 * (1 + (getFloat($("#mgn_l4").val()) / 100));
+    var retail_l5_o1 = net_l5_o1 * (1 + (getFloat($("#mgn_l5").val()) / 100));
+    var retail_l6_o1 = net_l6_o1 * (1 + (getFloat($("#mgn_l6").val()) / 100));
+    var retail_l7_o1 = net_l7_o1 * (1 + (getFloat($("#mgn_l7").val()) / 100));
 
-    var qty_l1 = parseInt($("#qty_l1").val());
-    var qty_l2 = parseInt($("#qty_l1").val());
-    var qty_l3 = parseInt($("#qty_l1").val());
-    var qty_l4 = parseInt($("#qty_l1").val());
-    var qty_l5 = parseInt($("#qty_l1").val());
-    var qty_l6 = parseInt($("#qty_l1").val());
-    var qty_l7 = parseInt($("#qty_l1").val());
+    var qty_l1 = getInt($("#qty_l1").val());
+    var qty_l2 = getInt($("#qty_l1").val());
+    var qty_l3 = getInt($("#qty_l1").val());
+    var qty_l4 = getInt($("#qty_l1").val());
+    var qty_l5 = getInt($("#qty_l1").val());
+    var qty_l6 = getInt($("#qty_l1").val());
+    var qty_l7 = getInt($("#qty_l1").val());
 
     var totCost_o1 = (qty_l1 * net_l1_o1) +
         (qty_l2 * net_l2_o1) +
@@ -42,6 +64,16 @@ function calculateAll(fieldID) {
         (qty_l5 * net_l5_o1) +
         (qty_l6 * net_l6_o1) +
         (qty_l7 * net_l7_o1);
+
+    var totRetail_o1 = (qty_l1 * retail_l1_o1) +
+        (qty_l2 * retail_l2_o1) +
+        (qty_l3 * retail_l3_o1) +
+        (qty_l4 * retail_l4_o1) +
+        (qty_l5 * retail_l5_o1) +
+        (qty_l6 * retail_l6_o1) +
+        (qty_l7 * retail_l7_o1);
+
+    var totMargin_o1 = ((totRetail_o1 - totCost_o1) / totCost_o1) * 100;
 
     $("#net_l1_o1").text('$' + net_l1_o1.toFixed(2));
     $("#net_l2_o1").text('$' + net_l2_o1.toFixed(2));
@@ -60,6 +92,8 @@ function calculateAll(fieldID) {
     $("#retail_l7_o1").text('$' + retail_l7_o1.toFixed(2));
 
     $("#totCost_o1").text('$' + totCost_o1.toFixed(2));
+    $("#totRetail_o1").text('$' + totRetail_o1.toFixed(2));
+    $("#totMargin_o1").text(totMargin_o1.toFixed(1) + '%');
 }
 
 function fillAllSalesMargin() {
