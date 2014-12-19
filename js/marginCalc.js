b1c3c7ad272c75127b67493816089d9d6dccbd84
mattodell <mrmattodell@gmail.com>
2014-12-19 13:17:08 -0500

    Added offcanvas menu, removed bottom button row, fixed bug in option 4 naming

--- js/marginCalc.js
+++ js/marginCalc.js
@@ -198,6 +198,7 @@ function clearData() {
     $("#discountType").val("(2-tier)");
 
     calculateAll();
+
 }
 
 // save the value of the passed input parameter to local storage
@@ -579,6 +580,10 @@ function fillAllDiscount(optionNum) {
     saveData("discount_l9_" + optionNum);
 }
 
+function openMarginTurnRef() {
+    window.open('margin-turns.html');
+}
+
 // Shows the print table div, collects all the data in a variable,
 //   hides the div and calls the popup function
 function printTable() {
@@ -588,6 +593,7 @@ function printTable() {
     $("#printDiv").hide();
     Popup(data);
 }
+
 //Method to popoulate the print table structure from the entered options and calculations.
 function PopulatePrintTable() {
 
